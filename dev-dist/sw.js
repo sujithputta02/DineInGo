78d53d27f87d1d38a605ecb89f803c1b6b12e64d
Sujith Putta <sujithputta@Sujiths-MacBook-Air.local>
2026-02-18 00:04:08 +0530

    Update service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-137dedbd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5evqs6idm8k"
+    "revision": "0.a43pc3op7ls"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
