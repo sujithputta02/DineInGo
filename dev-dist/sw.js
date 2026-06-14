2f20b4e50354ab78fe0f9fdad3ea5f6f8447f478
Sujith Putta <sujithputta02@gmail.com>
2026-06-14 12:43:56 +0530

    Chore: commit generated service worker file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-90f62ff3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.09ub4r77qfg"
+    "revision": "0.5l9c81bu6p4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
