c3f60a4cfb5eea151f4f4733555fbb918cd97b69
Sujith Putta <sujithputta02@gmail.com>
2026-07-26 12:16:37 +0530

    chore: update service worker cache revision for index.html preload changes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-90f62ff3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.svkqr1jpl8k"
+    "revision": "0.0brj13mcoe"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
